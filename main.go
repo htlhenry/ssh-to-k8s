33866b7552de742e156a75f875bd5ed5e25498d3
hantenglei <tengleihan@gmail.com>
2019-11-22 15:08:07 +0800

    Add log TimestampFormat

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-    log "github.com/sirupsen/logrus"
-    "ssh-to-k8s/cmd"
+	log "github.com/sirupsen/logrus"
+	"ssh-to-k8s/cmd"
 )
 
 func init() {
-    log.SetFormatter(&log.TextFormatter{
-    })
-    log.SetLevel(log.DebugLevel)
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	log.SetLevel(log.DebugLevel)
 }
 
 func main() {
-    cmd.Execute()
+	cmd.Execute()
 }
-
